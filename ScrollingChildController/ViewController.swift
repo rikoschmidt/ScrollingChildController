663f9bc9908a8355b77b660c268045c289196d28
Riko Schmidt <riko.schmidt@lidl.com>
2017-04-22 14:03:27 +0200

    Initial commit

---
+++ ScrollingChildController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollingChildController
+//
+//  Created by Riko Schmidt on 22/04/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
